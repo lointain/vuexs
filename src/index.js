e867bb1bb70e76eff696fd0a49254a11ad04eb5e
yaoyuan93 <cJkluio789>
2018-06-19 10:47:27 +0800

    适配nuxt, 修改bug

--- src/index.js
+++ src/index.js
@@ -1,295 +1,329 @@
-import Vue from 'vue'
+import _extends from 'babel-runtime/helpers/extends';
+import _typeof from 'babel-runtime/helpers/typeof';
+import _Object$keys from 'babel-runtime/core-js/object/keys';
+import _JSON$stringify from 'babel-runtime/core-js/json/stringify';
+import _Object$defineProperty from 'babel-runtime/core-js/object/define-property';
+import _regeneratorRuntime from 'babel-runtime/regenerator';
+import _asyncToGenerator from 'babel-runtime/helpers/asyncToGenerator';
+import Vue from 'vue';
 
 function install(vue, serviceDefineMap) {
   Vue.config.optionMergeStrategies.fetch = function (toVal, fromVal, aaa) {
-    return async function () {
-      if (toVal) toVal.apply(this, arguments)
-      if (fromVal) {
-        await fromVal.apply(this, arguments)
-        if (typeof process !== 'undefined' && process.server)
-          nuxtRender.apply(this, arguments)
-      }
+    return _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
+      var _args = arguments;
+      return _regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              if (toVal) toVal.apply(this, _args);
+
+              if (!fromVal) {
+                _context.next = 5;
+                break;
+              }
+
+              _context.next = 4;
+              return fromVal.apply(this, _args);
+
+            case 4:
+              if (typeof process !== 'undefined' && process.server) nuxtRender.apply(this, _args);
+
+            case 5:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, this);
+    }));
+  };
+  var serviceMap = {};
+  for (var key in serviceDefineMap) {
+    serviceDefineMap.name = key;
+    serviceMap[key] = createService(serviceDefineMap[key], key);
+  }
+  Vue.$service = serviceMap;
+  var mixin = function mixin() {
+    if (this.$parent) this.$service = this.$parent.$service;else this.$service = Vue.$service;
+    if (this.$options && this.$options.actions) for (var actionName in this.$options.actions) {
+      this[actionName] = this.$options.actions[actionName];
     }
-  }
-  const serviceMap = {}
-  for (const key in serviceDefineMap) {
-    serviceDefineMap.name = key
-    serviceMap[key] = createService(serviceDefineMap[key], key)
-  }
-  Vue.$service = serviceMap
-  const mixin = function () {
-    if (this.$parent)
-      this.$service = this.$parent.$service
-    else
-      this.$service = Vue.$service
-    if (this.$options && this.$options.actions)
-      for (const actionName in this.$options.actions) {
-        this[actionName] = this.$options.actions[actionName]
-      }
     if (this.$options && this.$options.services) {
-      for (const name in this.$options.services) {
-        const serviceName = this.$options.services[name]
-        this[name] = getService(serviceName)
+      for (var name in this.$options.services) {
+        var serviceName = this.$options.services[name];
+        this[name] = getService(serviceName);
       }
     }
-    if (this.$name) {
-    } else if (this.$options && this.$options.name) {
-      this.$name = this.$options.name
+    if (this.$name) {} else if (this.$options && this.$options.name) {
+      this.$name = this.$options.name;
     } else if (this.$vnode) {
-      this.$name = this.$vnode.tag
+      this.$name = this.$vnode.tag;
     } else {
-      this.$name = ''
+      this.$name = '';
     }
-  }
+  };
   vue.mixin({
-    fetch: function () {
-      if (this.$parent)
-        this.$service = this.$parent.$service
-      else
-        this.$service = Vue.$service
+    fetch: function fetch() {
+      if (this.$parent) this.$service = this.$parent.$service;else this.$service = Vue.$service;
     },
     beforeCreate: mixin
-  })
-  for (const key in serviceMap) {
-    const service = serviceMap[key]
-    if (service.service)
-      for (const key2 in service.service) {
-        Vue.$service[key][key2] = getService(key2)
-        if (devtoolHook)
-          Vue.$service[key][key2]._devtoolHook = devtoolHook
-      }
+  });
+  for (var _key in serviceMap) {
+    var _service = serviceMap[_key];
+    if (_service.service) for (var key2 in _service.service) {
+      Vue.$service[_key][key2] = getService(key2);
+      if (devtoolHook) Vue.$service[_key][key2]._devtoolHook = devtoolHook;
+    }
   }
 
   if (Vue.config.devtools) {
-    devtoolPlugin(serviceMap)
+    devtoolPlugin(serviceMap);
   }
 }
 
-const devtoolHook =
-  typeof window !== 'undefined' &&
-  window.__VUE_DEVTOOLS_GLOBAL_HOOK__
+var devtoolHook = typeof window !== 'undefined' && window.__VUE_DEVTOOLS_GLOBAL_HOOK__;
 
 function devtoolPlugin(serviceMap) {
   if (devtoolHook) {
-    const state = {}
-    const getters = {}
-    for (const key in serviceMap) {
-      const service = serviceMap[key]
-      state[key] = service.$data
-      for (const computedName in service._computedWatchers) {
-        let name = '[C]' + computedName
-        if (serviceTempMap.state[key] &&
-          serviceTempMap.state[key][computedName]) {
-          name += '(' + serviceTempMap.state[key][computedName] + ')'
-        }
-        Object.defineProperty(state[key], name, {
-          get: () => service[computedName],
-          enumerable: true
-        })
-      }
-    }
-    devtoolHook.emit('vuex:init', {state: state, getters: getters})
+    var _state = {};
+    var getters = {};
 
-    devtoolHook.on('vuex:travel-to-state', (targetState) => {
-      const serviceMap = Vue.$service
-      for (let stateName in targetState) {
-        if (stateName.indexOf('[C]') === 0) continue
-        const state = targetState[stateName]
-        for (let key in state) {
-          const value = state[key]
-          serviceMap[stateName][key] = value
+    var _loop = function _loop(key) {
+      var service = serviceMap[key];
+      _state[key] = service.$data;
+
+      var _loop2 = function _loop2(computedName) {
+        var name = '[C]' + computedName;
+        if (serviceTempMap.state[key] && serviceTempMap.state[key][computedName]) {
+          name += '(' + serviceTempMap.state[key][computedName] + ')';
+        }
+        _Object$defineProperty(_state[key], name, {
+          get: function get() {
+            return service[computedName];
+          },
+          enumerable: true
+        });
+      };
+
+      for (var computedName in service._computedWatchers) {
+        _loop2(computedName);
+      }
+    };
+
+    for (var key in serviceMap) {
+      _loop(key);
+    }
+    devtoolHook.emit('vuex:init', { state: _state, getters: getters });
+
+    devtoolHook.on('vuex:travel-to-state', function (targetState) {
+      var serviceMap = Vue.$service;
+      for (var stateName in targetState) {
+        if (stateName.indexOf('[C]') === 0) continue;
+        var _state2 = targetState[stateName];
+        for (var key in _state2) {
+          var value = _state2[key];
+          serviceMap[stateName][key] = value;
         }
       }
-    })
+    });
   }
 }
 
-let actionTempList = []
+var actionTempList = [];
 
 function devtoolsAction(fn, nameObj) {
-  const name = nameObj
-  return function (...args) {
-    const argsJson = []
-    for (let i = 0; i < args.length; i++) {
-      argsJson.push(JSON.stringify(args[i]))
+  var name = nameObj;
+  return function () {
+    for (var _len = arguments.length, args = Array(_len), _key2 = 0; _key2 < _len; _key2++) {
+      args[_key2] = arguments[_key2];
     }
-    let type = [name.service, name.action].join('.') + '(' +
-      [...argsJson].join(',') + ')'
-    if (serviceTempMap.action[name.service] &&
-      serviceTempMap.action[name.service][name.action]) {
+
+    var argsJson = [];
+    for (var i = 0; i < args.length; i++) {
+      argsJson.push(_JSON$stringify(args[i]));
+    }
+    var type = [name.service, name.action].join('.') + '(' + [].concat(argsJson).join(',') + ')';
+    if (serviceTempMap.action[name.service] && serviceTempMap.action[name.service][name.action]) {
       // name += '(' + serviceTempMap.state[key][computedName] + ')'
-      actionTempList.push(type)
+      actionTempList.push(type);
     } else {
-      actionTempList.push(type)
-      if (this) type = '[' + this.$name + ']' + actionTempList.join('->')
+      actionTempList.push(type);
+      if (this) type = '[' + this.$name + ']' + actionTempList.join('->');
       if (devtoolHook) {
         Vue.nextTick(function () {
           devtoolHook.emit('vuex:mutation', {
             type: type,
             payload: args
-          }, Vue.$service)
-        })
+          }, Vue.$service);
+        });
       }
-      actionTempList = []
+      actionTempList = [];
     }
-    const returnFn = fn.apply(null, arguments)
+    var returnFn = fn.apply(null, arguments);
 
     if (returnFn && returnFn.toString() === '[object Promise]') {
-      type = 'promise:' + type
+      type = 'promise:' + type;
     }
-    return returnFn
-  }
+    return returnFn;
+  };
 }
 
 function createService(service, serviceName) {
-  const option = {
+  var option = {
     data: {},
     computed: {}
-  }
-  if (service.computed)
-    option.computed = service.computed
+  };
+  if (service.computed) option.computed = service.computed;
   if (service.state) {
     if (typeof window !== 'undefined' && window.__NUXT__ && window.__NUXT__.service && window.__NUXT__.service[serviceName]) {
-      option.data = window.__NUXT__.service[serviceName]
+      option.data = window.__NUXT__.service[serviceName];
     }
-    for (const key in service.state) {
-      if (typeof service.state[key] === 'function')
-        option.computed[key] = service.state[key]
-      else if (!option.data[key])
-        option.data[key] = service.state[key]
+    for (var key in service.state) {
+      if (typeof service.state[key] === 'function') option.computed[key] = service.state[key];else if (!option.data[key]) option.data[key] = service.state[key];
     }
   }
 
-  if (service.watch)
-    option.watch = service.watch
-  const _vm = new Vue(option)
-  if (service.actions)
-    for (const key in service.actions) {
-      const actionFn = service.actions[key].bind(_vm)
-      _vm[key] = Vue.config.devtools ? devtoolsAction(actionFn, {
-        action: key,
-        service: serviceName
-      }) : actionFn
-    }
-  if (service.utils)
-    _vm.utils = service.utils
-  if (service.service)
-    _vm.service = service.service
-  _vm.$name = serviceName
-  return _vm
+  if (service.watch) option.watch = service.watch;
+  var _vm = new Vue(option);
+  if (service.actions) for (var _key3 in service.actions) {
+    var actionFn = service.actions[_key3].bind(_vm);
+    _vm[_key3] = Vue.config.devtools ? devtoolsAction(actionFn, {
+      action: _key3,
+      service: serviceName
+    }) : actionFn;
+  }
+  if (service.utils) _vm.utils = service.utils;
+  if (service.service) _vm.service = service.service;
+  _vm.$name = serviceName;
+  return _vm;
 }
 
 function getService(serviceName) {
-  const service = Vue.$service[serviceName]
-  if (typeof service === 'undefined')
-    console.error('cant find service by name:' + serviceName)
-  else
-    return service
+  var service = Vue.$service[serviceName];
+  if (typeof service === 'undefined') console.error('cant find service by name:' + serviceName);else return service;
 }
 
 function normalizeMap(map) {
-  return Array.isArray(map)
-    ? map.map(key => ({key, val: key}))
-    : Object.keys(map).map(key => ({key, val: map[key]}))
+  return Array.isArray(map) ? map.map(function (key) {
+    return { key: key, val: key };
+  }) : _Object$keys(map).map(function (key) {
+    return { key: key, val: map[key] };
+  });
 }
 
 function mapState(serviceName, stateList) {
-  const res = {}
-  normalizeMap(stateList).forEach(({key, val}) => {
+  var res = {};
+  normalizeMap(stateList).forEach(function (_ref2) {
+    var key = _ref2.key,
+        val = _ref2.val;
+
     res[key] = function () {
-      return getService(serviceName)[val]
-    }
-  })
-  return res
+      return getService(serviceName)[val];
+    };
+  });
+  return res;
 }
 
 function mapAction(serviceName, stateList) {
-  const res = {}
-  normalizeMap(stateList).forEach(({key, val}) => {
-    res[key] = function (...args) {
-      getService(serviceName)[val].call(this, ...args)
-    }
-  })
-  return res
+  var res = {};
+  normalizeMap(stateList).forEach(function (_ref3) {
+    var key = _ref3.key,
+        val = _ref3.val;
+
+    res[key] = function () {
+      var _getService$val;
+
+      for (var _len2 = arguments.length, args = Array(_len2), _key4 = 0; _key4 < _len2; _key4++) {
+        args[_key4] = arguments[_key4];
+      }
+
+      (_getService$val = getService(serviceName)[val]).call.apply(_getService$val, [this].concat(args));
+    };
+  });
+  return res;
 }
 
 function state(serviceName) {
   return function (target, name, descriptor) {
     if (typeof target[name] === 'string') {
-      if (typeof target.computed === 'undefined')
-        target.computed = {}
+      if (typeof target.computed === 'undefined') target.computed = {};
       target.computed[name] = function () {
-        return getService(serviceName)[descriptor.value]
-      }
-    } else if (typeof target[name] === 'object') {
-      const valueList = target[name]
-      if (typeof target.computed === 'undefined')
-        target.computed = {}
-      target.computed = {
-        ...target.computed, ...mapState(serviceName, valueList)
-      }
+        return getService(serviceName)[descriptor.value];
+      };
+    } else if (_typeof(target[name]) === 'object') {
+      var valueList = target[name];
+      if (typeof target.computed === 'undefined') target.computed = {};
+      target.computed = _extends({}, target.computed, mapState(serviceName, valueList));
       if (Vue.config.devtools) {
         if (target.name) {
-          normalizeMap(valueList).forEach(({key, val}) => {
-            setServiceTempState(target.name, key, serviceName + '.' + val)
-          })
+          normalizeMap(valueList).forEach(function (_ref4) {
+            var key = _ref4.key,
+                val = _ref4.val;
+
+            setServiceTempState(target.name, key, serviceName + '.' + val);
+          });
         }
       }
     }
-    delete target[name]
-  }
+    delete target[name];
+  };
 }
 
-const serviceTempMap = {
+var serviceTempMap = {
   state: {},
   action: {}
-}
+};
 
 function setServiceTempState(service, key, name) {
   if (typeof serviceTempMap.state[service] === 'undefined') {
-    serviceTempMap.state[service] = {}
+    serviceTempMap.state[service] = {};
   }
-  serviceTempMap.state[service][key] = name
+  serviceTempMap.state[service][key] = name;
 }
 
 function setServiceTempAction(service, key, name) {
   if (typeof serviceTempMap.action[service] === 'undefined') {
-    serviceTempMap.action[service] = {}
+    serviceTempMap.action[service] = {};
   }
-  serviceTempMap.action[service][key] = name
+  serviceTempMap.action[service][key] = name;
 }
 
 function action(serviceName) {
   return function (target, name, descriptor) {
     if (typeof target[name] === 'string') {
-      if (typeof target.actions === 'undefined')
-        target.actions = {}
-      target.actions[name] = function (...args) {
-        getService(serviceName)[descriptor.value].call(this, ...args)
-      }
-    } else if (typeof target[name] === 'object') {
-      const valueList = target[name]
-      if (typeof target.actions === 'undefined')
-        target.actions = {}
-      target.actions = {...target.actions, ...mapAction(serviceName, valueList)}
+      if (typeof target.actions === 'undefined') target.actions = {};
+      target.actions[name] = function () {
+        var _getService$descripto;
+
+        for (var _len3 = arguments.length, args = Array(_len3), _key5 = 0; _key5 < _len3; _key5++) {
+          args[_key5] = arguments[_key5];
+        }
+
+        (_getService$descripto = getService(serviceName)[descriptor.value]).call.apply(_getService$descripto, [this].concat(args));
+      };
+    } else if (_typeof(target[name]) === 'object') {
+      var valueList = target[name];
+      if (typeof target.actions === 'undefined') target.actions = {};
+      target.actions = _extends({}, target.actions, mapAction(serviceName, valueList));
       if (Vue.config.devtools) {
         if (target.name) {
-          normalizeMap(valueList).forEach(({key, val}) => {
-            setServiceTempAction(target.name, key, serviceName + '.' + val)
-          })
+          normalizeMap(valueList).forEach(function (_ref5) {
+            var key = _ref5.key,
+                val = _ref5.val;
+
+            setServiceTempAction(target.name, key, serviceName + '.' + val);
+          });
         }
       }
-      console.log(serviceTempMap.action)
+      console.log(serviceTempMap.action);
     }
-    delete target[name]
-  }
+    delete target[name];
+  };
 }
 
 function service(target, name, descriptor) {
-  if (typeof target.services === 'undefined')
-    target.services = {}
-  const serviceName = target[name]
-  target.services[name] = serviceName
+  if (typeof target.services === 'undefined') target.services = {};
+  var serviceName = target[name];
+  target.services[name] = serviceName;
   // const service = getService(serviceName)
   // target[name] = function(){
   //   return getService(serviceName)
@@ -301,35 +335,31 @@ function service(target, name, descriptor) {
 }
 
 function nuxtRender(ctx) {
-  ctx.beforeNuxtRender(({Components, nuxtState}) => {
+  ctx.beforeNuxtRender(function (_ref6) {
+    var Components = _ref6.Components,
+        nuxtState = _ref6.nuxtState;
+
     if (Components.length > 0) {
-      const component = Components[0]
+      var component = Components[0];
       if (component.super && component.super.$service) {
-        const serviceList = component.super.$service
-        const stateList = {}
-        for (let serviceName in serviceList) {
-          stateList[serviceName] = {}
-          const service = serviceList[serviceName]
-          const dataMap = service._data
-          for (let dataName in dataMap) {
-            stateList[serviceName][dataName] = dataMap[dataName]
+        var serviceList = component.super.$service;
+        var stateList = {};
+        for (var serviceName in serviceList) {
+          stateList[serviceName] = {};
+          var _service2 = serviceList[serviceName];
+          var dataMap = _service2._data;
+          for (var dataName in dataMap) {
+            stateList[serviceName][dataName] = dataMap[dataName];
           }
         }
-        nuxtState.service = stateList
+        nuxtState.service = stateList;
       }
     }
-  })
+  });
 }
 
-export {
-  action,
-  state,
-  service,
-  mapState,
-  mapAction,
-  nuxtRender
-}
+export { action, state, service, mapState, mapAction, nuxtRender };
 export default {
-  install,
+  install: install,
   get: getService
-}
+};
